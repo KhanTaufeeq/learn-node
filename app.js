1883b6cbf192890f2238f0db6d27151a4e8a315b
KhanTaufeeq <taufeeqyouth@gmail.com>
2024-12-26 08:55:06 +0530

    started the basics

---
+++ app.js
@@ -0,0 +1,9 @@
+const num = 200;
+
+if(num > 100){
+  console.log('number is greater than 100')
+}else if(num < 100){
+  console.log('number is less than 100')
+}else{
+  console.log('number is 100')
+}
